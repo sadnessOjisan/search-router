1540c1337e11cc2d721b03800a6c7b2f08c24240
sadnessOjisan <sadness.ojisan@gmail.com>
2020-10-04 00:10:37 +0900

    dataを追加

---
+++ src/helper/data.ts
@@ -0,0 +1,84 @@
+import { DataType } from "../type";
+
+export const data: DataType = [
+  {
+    area: "千葉",
+    food: "カレー",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "横浜",
+    food: "ラーメン",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "千葉",
+    food: "カレー",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "東京",
+    food: "カレー",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "千葉",
+    food: "ラーメン",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "横浜",
+    food: "カレー",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "東京",
+    food: "カレー",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "千葉",
+    food: "カレー",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "千葉",
+    food: "カレー",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "横浜",
+    food: "ラーメン",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "千葉",
+    food: "カレー",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "千葉",
+    food: "カレー",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "千葉",
+    food: "ラーメン",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "東京",
+    food: "ラーメン",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "千葉",
+    food: "カレー",
+    name: Math.random().toString(32).substring(2),
+  },
+  {
+    area: "東京",
+    food: "ラーメン",
+    name: Math.random().toString(32).substring(2),
+  },
+];
--- src/type.ts
+++ src/type.ts
@@ -2,3 +2,6 @@ export type AreaType = "東京" | "横浜" | "千葉";
 export type FoodType = "カレー" | "ラーメン";
 export type AreaFormValidType = AreaType | "ALL";
 export type FoodFormValidType = FoodType | "ALL";
+
+type DataItemType = { area: AreaType; food: FoodType; name: string };
+export type DataType = DataItemType[];
