4a1c3d8040cbdda79c037bbed42bfc0e63d727c5
sadnessOjisan <sadness.ojisan@gmail.com>
2020-10-04 15:19:28 +0900

    fix workflows

--- src/pages/reducerDetail.tsx
+++ src/pages/reducerDetail.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useHistory } from "react-router-dom";
 import { DataItemType } from "../type";
 import { isValidDataItem } from "../helper/util";
 
@@ -13,13 +13,22 @@ interface Props extends PassedProps {
 
 const Component: React.FC<Props> = (props) => {
   const { routerState } = props;
+  const history = useHistory();
+
   return (
     <div className={props.className}>
       <div className="title">{routerState.name}</div>
       <div className="infoRow">
         {routerState.area}/{routerState.food}
       </div>
-      <Link to="/reducer-cards">戻る</Link>
+      <Link to="/reducer-cards">戻る(link)</Link>
+      <button
+        onClick={() => {
+          history.goBack();
+        }}
+      >
+        戻る(go back)
+      </button>
     </div>
   );
 };
@@ -40,6 +49,7 @@ const ContainerComponent: React.FC<PassedProps> = (props) => {
   const location = useLocation();
   const routerState = location.state;
   if (!isValidDataItem(routerState)) throw new Error("invalid data");
+
   return <StyledComponent routerState={routerState}></StyledComponent>;
 };
 
